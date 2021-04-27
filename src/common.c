d23868e2662a788de16ac2ef4b05f2a4e1efde3d
Michael <mokchira@gmail.com>
2021-04-27 12:42:03 -0400

    update

--- src/common.c
+++ src/common.c
@@ -1,10 +1,15 @@
 #include "common.h"
 #include <hell/common.h>
+#include <hell/cmd.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <obsidian/v_video.h>
 
 #define MAX_PRINT_MSG 256
 #define HEADER "OBSIDIAN: "
+
+#define STR_100_MB "104857600"
+#define STR_256_MB "268435456"
 
 void obdn_Announce(const char* fmt, ...)
 {
@@ -16,3 +21,15 @@ void obdn_Announce(const char* fmt, ...)
     hell_Print("%s%s", HEADER, msg);
 }
 
+void obdn_Init()
+{
+    Obdn_V_Config cfg;
+    cfg.memorySizes.hostGraphicsBufferMemorySize   = hell_c_GetVar("hstGraphicsBufferMemorySize", STR_100_MB, HELL_C_VAR_ARCHIVE_BIT)->value;
+    cfg.memorySizes.deviceGraphicsBufferMemorySize = hell_c_GetVar("devGraphicsBufferMemorySize", STR_100_MB, HELL_C_VAR_ARCHIVE_BIT)->value;
+    cfg.memorySizes.deviceGraphicsImageMemorySize  =  hell_c_GetVar("devGraphicsImageSize", STR_256_MB, HELL_C_VAR_ARCHIVE_BIT)->value;
+#ifdef NDEBUG
+    cfg.validationEnabled = false;
+#else
+    cfg.validationEnabled = true;
+#endif
+}
