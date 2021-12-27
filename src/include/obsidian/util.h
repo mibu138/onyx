faa9976347aa78b205b04331e8cfa1c072d1502c
mokchira <mokchira@gmail.com>
2021-12-26 19:24:56 -0500

    Restructured project directories. Attempting to use in-source headers because it seems that faking in-source headers, by specifying include/obsidian as an include dir for the library target, leads to issues when a project using obsidian shares a name with one of the headers. For instance, if that project has a header called common.h that it is truly in source, and then we include obsidian/obsidian, which has a include "common.h" statement, it seems this get confused and does not include the obsidian common but the one in the newer project, since its "common.h" is actually a <common.h>. Not sure if this makes sense but a name collision was definitely the source of some issues... also it is possible that it is just confusing clangd.

--- src/include/obsidian/util.h
+++
@@ -1,8 +0,0 @@
-#ifndef V_UTIL_H
-#define V_UTIL_H
-
-#include "video.h"
-
-void obdn_PrintFormatProperties(const Obdn_Instance* instance, VkFormat);
-
-#endif /* end of include guard: V_UTILS_H */
--- src/lib/render.c
+++
@@ -1,8 +0,0 @@
-#include "scene.h"
-
-// TODO: we should implement a way to specify the offscreen format
-static const VkFormat offscreenColorFormat = VK_FORMAT_R32G32B32A32_SFLOAT;
-static const VkFormat depthFormat          = VK_FORMAT_D32_SFLOAT;
-
-VkFormat obdn_r_GetOffscreenColorFormat(void) { return offscreenColorFormat; }
-VkFormat obdn_r_GetDepthFormat(void)          { return depthFormat; }
